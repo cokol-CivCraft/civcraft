834d72525e5f4c7248df4cb6759f7f06767f3489
PashkovD <dimon2006763@gmail.com>
2023-08-03 22:47:58 +0500

    Refactored config to arenas

--- src/java/com/avrgaming/civcraft/config/ConfigArena.java
+++ src/java/com/avrgaming/civcraft/config/ConfigArena.java
@@ -3,10 +3,13 @@ package com.avrgaming.civcraft.config;
 import com.avrgaming.civcraft.main.CivLog;
 import com.avrgaming.civcraft.util.BlockCoord;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
+import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class ConfigArena {
     public String id;
@@ -18,95 +21,86 @@ public class ConfigArena {
 
     public static void loadConfig(FileConfiguration cfg, Map<String, ConfigArena> config_arenas) {
         config_arenas.clear();
-
-        List<Map<?, ?>> cottage_list = cfg.getMapList("arenas");
-
-        for (Map<?, ?> cl : cottage_list) {
-            @SuppressWarnings("unchecked")
-            List<Map<?, ?>> teams_map_list = (List<Map<?, ?>>) cl.get("teams");
-
+        for (File file : Objects.requireNonNull(new File("arenas/").listFiles(File::isDirectory))) {
             ConfigArena arena = new ConfigArena();
-            arena.id = (String) cl.get("id");
-            arena.name = (String) cl.get("name");
-            arena.world_source = (String) cl.get("world_folder");
-            arena.control_block_hp = (Integer) cl.get("control_block_hp");
-
-            if (teams_map_list != null) {
-                arena.teams = new LinkedList<>();
-                for (Map<?, ?> tm : teams_map_list) {
-                    ConfigArenaTeam team = new ConfigArenaTeam();
-
-                    team.number = (Integer) tm.get("number");
-                    team.name = (String) tm.get("name");
-
-                    /* Set up control blocks. */
-                    team.controlPoints = new LinkedList<>();
-                    List<?> someList = (List<?>) tm.get("controlblocks");
-                    for (Object obj : someList) {
-                        String[] coords = ((String) obj).split(",");
-                        BlockCoord bcoord = new BlockCoord(arena.world_source,
-                                Integer.parseInt(coords[0]),
-                                Integer.parseInt(coords[1]),
-                                Integer.parseInt(coords[2]));
-
-
-                        team.controlPoints.add(bcoord);
-                    }
-
-                    /* Set up revive points. */
-                    team.revivePoints = new LinkedList<>();
-                    someList = (List<?>) tm.get("revivepoints");
-                    for (Object obj : someList) {
-                        String[] coords = ((String) obj).split(",");
-                        BlockCoord bcoord = new BlockCoord(arena.world_source,
-                                Integer.parseInt(coords[0]),
-                                Integer.parseInt(coords[1]),
-                                Integer.parseInt(coords[2]));
-
-
-                        team.revivePoints.add(bcoord);
-                    }
-
-                    /* Set up respawn points. */
-                    team.respawnPoints = new LinkedList<>();
-                    someList = (List<?>) tm.get("respawnpoints");
-                    for (Object obj : someList) {
-                        String[] coords = ((String) obj).split(",");
-                        BlockCoord bcoord = new BlockCoord(arena.world_source,
-                                Integer.parseInt(coords[0]),
-                                Integer.parseInt(coords[1]),
-                                Integer.parseInt(coords[2]));
-
-
-                        team.respawnPoints.add(bcoord);
-                    }
-
-                    /* Set up chest points. */
-                    team.chests = new LinkedList<>();
-                    someList = (List<?>) tm.get("chests");
-                    for (Object obj : someList) {
-                        String[] coords = ((String) obj).split(",");
-                        BlockCoord bcoord = new BlockCoord(arena.world_source,
-                                Integer.parseInt(coords[0]),
-                                Integer.parseInt(coords[1]),
-                                Integer.parseInt(coords[2]));
-
-
-                        team.chests.add(bcoord);
-                    }
-
-                    String respawnSignStr = (String) tm.get("respawnsign");
-                    String[] respawnSplit = respawnSignStr.split(",");
-                    team.respawnSign = new BlockCoord(arena.world_source,
-                            Integer.parseInt(respawnSplit[0]),
-                            Integer.parseInt(respawnSplit[1]),
-                            Integer.parseInt(respawnSplit[2]));
-
-                    arena.teams.add(team);
-                }
+            arena.id = file.getName();
+            File config_file = new File("arenas/" + arena.id + "/config.yml");
+            if (!config_file.exists()) {
+                continue;
             }
+            FileConfiguration config = YamlConfiguration.loadConfiguration(config_file);
+            arena.name = config.getString("name");
+            arena.world_source = arena.id;
+            arena.control_block_hp = config.getInt("control_block_hp");
+
+            arena.teams = new LinkedList<>();
+            for (Map<?, ?> tm : config.getMapList("teams")) {
+                ConfigArenaTeam team = new ConfigArenaTeam();
+
+                team.number = (Integer) tm.get("number");
+                team.name = (String) tm.get("name");
+
+                /* Set up control blocks. */
+                team.controlPoints = new LinkedList<>();
+                for (Object obj : (List<?>) tm.get("controlblocks")) {
+                    String[] coords = ((String) obj).split(",");
+                    BlockCoord bcoord = new BlockCoord(arena.world_source,
+                            Integer.parseInt(coords[0]),
+                            Integer.parseInt(coords[1]),
+                            Integer.parseInt(coords[2]));
 
 
+                    team.controlPoints.add(bcoord);
+                }
+
+                /* Set up revive points. */
+                team.revivePoints = new LinkedList<>();
+                for (Object obj : (List<?>) tm.get("revivepoints")) {
+                    String[] coords = ((String) obj).split(",");
+                    BlockCoord bcoord = new BlockCoord(arena.world_source,
+                            Integer.parseInt(coords[0]),
+                            Integer.parseInt(coords[1]),
+                            Integer.parseInt(coords[2]));
+
+
+                    team.revivePoints.add(bcoord);
+                }
+
+                /* Set up respawn points. */
+                team.respawnPoints = new LinkedList<>();
+                for (Object obj : (List<?>) tm.get("respawnpoints")) {
+                    String[] coords = ((String) obj).split(",");
+                    BlockCoord bcoord = new BlockCoord(arena.world_source,
+                            Integer.parseInt(coords[0]),
+                            Integer.parseInt(coords[1]),
+                            Integer.parseInt(coords[2]));
+
+
+                    team.respawnPoints.add(bcoord);
+                }
+
+                /* Set up chest points. */
+                team.chests = new LinkedList<>();
+                for (Object obj : (List<?>) tm.get("chests")) {
+                    String[] coords = ((String) obj).split(",");
+                    BlockCoord bcoord = new BlockCoord(arena.world_source,
+                            Integer.parseInt(coords[0]),
+                            Integer.parseInt(coords[1]),
+                            Integer.parseInt(coords[2]));
+
+
+                    team.chests.add(bcoord);
+                }
+
+                String respawnSignStr = (String) tm.get("respawnsign");
+                String[] respawnSplit = respawnSignStr.split(",");
+                team.respawnSign = new BlockCoord(arena.world_source,
+                        Integer.parseInt(respawnSplit[0]),
+                        Integer.parseInt(respawnSplit[1]),
+                        Integer.parseInt(respawnSplit[2]));
+
+                arena.teams.add(team);
+            }
             config_arenas.put(arena.id, arena);
         }
 
