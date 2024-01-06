ea9368b5ab93112b1d4e51b671867e33d9d60eb3
PashkovD <dimon2006763@gmail.com>
2024-01-06 20:37:28 +0500

    More

--- src/java/com/avrgaming/civcraft/provider/DefaultMaterialProvider.java
+++ src/java/com/avrgaming/civcraft/provider/DefaultMaterialProvider.java
@@ -2,8 +2,12 @@ package com.avrgaming.civcraft.provider;
 
 import com.avrgaming.civcraft.config.CivSettings;
 import com.avrgaming.civcraft.config.ConfigMaterial;
+import org.bukkit.DyeColor;
+import org.bukkit.GrassSpecies;
 import org.bukkit.Material;
 import org.bukkit.SandstoneType;
+import org.bukkit.material.Dye;
+import org.bukkit.material.LongGrass;
 import org.bukkit.material.MaterialData;
 import org.bukkit.material.Sandstone;
 
@@ -11,6 +15,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("unused")
 @ParametersAreNonnullByDefault
 public class DefaultMaterialProvider {
     public static final List<ConfigMaterial> entries = new ArrayList<>();
@@ -73,6 +78,94 @@ public class DefaultMaterialProvider {
             )
                     .tradeValue(0.5)
                     .category("<lightgreen>Tier 1 Materials").end();
+    public static final ConfigMaterial CHROMIUM_ORE =
+            material(
+                    "mat_chromium_ore",
+                    "Chromium Ore",
+                    new MaterialData(Material.GHAST_TEAR)
+            )
+                    .tradeValue(0.1)
+                    .category("<lightgreen>Tier 1 Materials").end();
+
+    public static final ConfigMaterial TUNGSTEN_ORE =
+            material(
+                    "mat_tungsten_ore",
+                    "Tungsten Ore",
+                    new MaterialData(Material.FIREBALL))
+                    .tradeValue(0.5)
+                    .noRightClick().category("<lightgreen>Tier 1 Materials").end();
+
+    public static final ConfigMaterial POND_SCUM =
+            material(
+                    "mat_pond_scum",
+                    "Pond Scum",
+                    new Dye(DyeColor.MAGENTA))
+                    .tradeValue(0.1)
+                    .noRightClick().category("<lightpurple>Fishing Junk").end();
+
+    public static final ConfigMaterial TANGLED_STRING =
+            material(
+                    "mat_tangled_string",
+                    "Tangled String",
+                    new MaterialData(Material.WEB))
+                    .tradeValue(0.1)
+                    .noRightClick().category("<lightpurple>Fishing Junk").end();
+
+    public static final ConfigMaterial SEAWEED =
+            material(
+                    "mat_seaweed",
+                    "Seaweed",
+                    new LongGrass(GrassSpecies.NORMAL))
+                    .tradeValue(0.1)
+                    .noRightClick().category("<lightpurple>Fishing Junk").end();
+
+    public static final ConfigMaterial MINNOWS =
+            material(
+                    "mat_minnows",
+                    "Minnows",
+                    new MaterialData(Material.MELON_SEEDS))
+                    .tradeValue(0.5)
+                    .noRightClick().category("<lightpurple>Fishing Junk").end();
+
+    public static final ConfigMaterial TADPOLE =
+            material(
+                    "mat_tadpole",
+                    "Tadpole",
+                    new MaterialData(Material.SPIDER_EYE))
+                    .tradeValue(0.2)
+                    .noRightClick().category("<lightpurple>Fishing Junk").end();
+
+    public static final ConfigMaterial FISH_FISH =
+            material(
+                    "mat_fish_fish",
+                    "Fish",
+                    new MaterialData(Material.RAW_FISH))
+                    .tradeValue(5.0)
+                    .noRightClick().category("<lightgreen>Fish Tier 1").end();
+
+    public static final ConfigMaterial FISH_SALMON =
+            material(
+                    "mat_fish_salmon",
+                    "Salmon",
+                    new MaterialData(Material.RAW_FISH, (byte) 1))
+                    .tradeValue(5.0)
+                    .noRightClick().category("<lightgreen>Fish Tier 1").end();
+
+    public static final ConfigMaterial FISH_CLOWNFISH =
+            material(
+                    "mat_fish_clownfish",
+                    "Clownfish",
+                    new MaterialData(Material.RAW_FISH, (byte) 2))
+                    .tradeValue(5.0)
+                    .noRightClick().category("<lightgreen>Fish Tier 1").end();
+
+    public static final ConfigMaterial FISH_PUFFERFISH =
+            material(
+                    "mat_fish_pufferfish",
+                    "Pufferfish",
+                    new MaterialData(Material.RAW_FISH, (byte) 3))
+                    .tradeValue(5.0)
+                    .noRightClick().category("<lightgreen>Fish Tier 1").end();
 
     private static Builder material(String id, String name, MaterialData data) {
         return new Builder(id, name, data);
@@ -92,6 +185,9 @@ public class DefaultMaterialProvider {
             this.data = data;
         }
 
+        public Builder noRightClick() {
+            return this;
+        }
         public Builder tradeValue(double value) {
             this.tradeValue = value;
             return this;
@@ -108,7 +204,7 @@ public class DefaultMaterialProvider {
             conf.item_id = data.getItemType();
             conf.item_data = data.getData();
             conf.category = this.category;
-            if (!tradeValue.equals(null)) {
+            if (tradeValue != null) {
                 conf.tradeable = true;
                 conf.tradeValue = this.tradeValue;
             }
